fd0ab882fe04fdac5a6a293847078794549aaf49
shlomo lifschitz <shinlamed@gmail.com>
2022-09-13 10:58:24 +0300

    handle-change-function

--- src/cmps/create-bot/create-bot-radios.jsx
+++ src/cmps/create-bot/create-bot-radios.jsx
@@ -1,16 +1,17 @@
-export const CreateBotRadios = () => {
+export const CreateBotRadios = (props) => {
+    const {handleChange } = props
     return (
         <div className="radio-wrapper">
             <fieldset>
                 <legend>Gender</legend>
                 <div className="flex pad-10px">
                     <div>
-                        <input type="radio" id="male" name="gender" value="male" />
+                        <input type="radio" onChange={handleChange} id="male" name="gender" value="male" />
                         <label htmlFor="male">Male</label>
                     </div>
 
                     <div>
-                        <input type="radio" id="female" name="gender" value="female" />
+                        <input type="radio" onChange={handleChange} id="female" name="gender" value="female" />
                         <label htmlFor="female">Female</label>
                     </div>
                 </div>
--- src/pages/create-bot.jsx
+++ src/pages/create-bot.jsx
@@ -20,19 +20,26 @@ export const CreateBot = (props) => {
         console.log('submiting')
     }
 
-    const handleChange = ({ target }) => {
+    const handleChange = (ev) => {
+        let timeOut
+        const { target } = ev
         const val = target.value
         const field = target.name
-        setBot({ ...bot, [field]: val })
+        clearTimeout(timeOut)
+         timeOut = setTimeout(() => {
+            setBot({ ...bot, [field]: val })
+            console.log('Hola');
+        }, 3000)
+        
     }
-    console.log(bot)
+    console.log(bot);
     return (
         <div className="fake-section flex">
             <section className="create-bot align-center pad-10px">
                 <form onSubmit={onSubmit} className="flex column pad-10px">
                     <CreateBotInputs handleChange={handleChange} />
-                    <CreateBotRadios />
-                    <CreateBotTextareas />
+                    <CreateBotRadios handleChange={handleChange} />
+                    <CreateBotTextareas handleChange={handleChange} />
                 </form>
             </section>
 
